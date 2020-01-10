bdac4b8c046a8c29e14dac7b9f4457bb97d51ce8
Mike Plis <michael.plis01@gmail.com>
2020-01-09 19:08:45 -0500

    Finished Part 5
    * Added route to user service for creating sessions
    * Added mutation to api-gateway to create sessions

--- api-gateway/src/graphql/typeDefs.js
+++ api-gateway/src/graphql/typeDefs.js
@@ -1,12 +1,15 @@
 import { gql } from "apollo-server";
 
 const typeDefs = gql`
+  scalar Date
+
   type Query {
     listings: [Listing!]!
   }
 
   type Mutation {
     createUser(email: String!, password: String!): User!
+    createUserSession(email: String!, password: String!): UserSession!
   }
 
   type Listing {
@@ -18,6 +21,13 @@ const typeDefs = gql`
   type User {
     email: String!
     id: ID!
+  }
+
+  type UserSession {
+    id: ID!
+    user: User!
+    createdAt: Date!
+    expiresAt: Date!
   }
 `;
 
